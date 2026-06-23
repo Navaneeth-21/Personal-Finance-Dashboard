a26935796c1ac6264dfb8cc35bbb543a2d1feae4
Navaneeth Gade <navaneethgade07@gmail.com>
2026-06-23 19:26:06 +0530

    Component: Adding Search operation for transactions

---
+++ src/components/TransactionSearch.tsx
@@ -0,0 +1,23 @@
+type TransactionSearchProps = {
+  searchTerm: string;
+  onSearchChange: (value: string) => void;
+};
+
+function TransactionSearch({
+  searchTerm,
+  onSearchChange,
+}: TransactionSearchProps) {
+  return (
+    <section className="mt-8">
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(event) => onSearchChange(event.target.value)}
+        placeholder="Search by title or category or type ..."
+        className="w-72 rounded-xl border border-slate-500 bg-white px-3 py-3 outline-none focus:border-slate-950"
+      />
+    </section>
+  );
+}
+
+export default TransactionSearch;
